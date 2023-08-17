0453f93ebb48630f4cb3bede9aacffe3b30b9208
Dhananjayan <dhananjayan.s.n@doodleblue.in>
2023-08-17 12:08:51 +0530

    Latest build

---
+++ dist/assets/ThemeProvider-422010ce.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-affb1123.js";const r=["xxl","xl","lg","md","sm","xs"],x="xs",o=e.createContext({prefixes:{},breakpoints:r,minBreakpoint:x});function i(t,s){const{prefixes:n}=e.useContext(o);return t||n[s]||s}function a(){const{dir:t}=e.useContext(o);return t==="rtl"}export{a,i as u};
